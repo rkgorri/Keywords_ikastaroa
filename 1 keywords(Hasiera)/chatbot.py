9c9864bf1ed90c9ae517bfca09bc3d3267d964bd
rkgorri <109732468+rkgorri@users.noreply.github.com>
2024-05-13 17:39:52 +0000

    aurre-prozesaketa

--- 1 keywords(Hasiera)/chatbot.py
+++ 1 keywords(Hasiera)/chatbot.py
@@ -27,8 +27,16 @@ def clean_text(query):
     if sentence.startswith('¿'):
         sentence = sentence[1:]
     
-    tildes = ['á','é','í','ó','ú']
-    vocales = ['a','e','i','o','u']
+    #Esaldiari azkenengo ! kendu
+    if sentence.endswith('!'):
+        sentence = sentence[:-1]
+    
+    #Esaldiari hasierako ¡ kendu
+    if sentence.startswith('¡'):
+        sentence = sentence[1:]
+    
+    tildes = ['á','é','í','ó','ú','ü']
+    vocales = ['a','e','i','o','u','u']
    
     for idx, vocal in enumerate(vocales):        
         sentence = re.sub(tildes[idx], vocal, sentence)
